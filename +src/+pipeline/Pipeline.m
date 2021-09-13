3f18a66c4d2cdadb17bd9dd38278fbea82a4e858
Drew Yang <drew@vathes.com>
2021-09-13 21:37:59 +0000

    major refactoring

--- +src/+pipeline/Pipeline.m
+++ +src/+pipeline/Pipeline.m
@@ -3,46 +3,24 @@ classdef Pipeline < handle
     %   Detailed explanation goes here
     
     properties
-        pipeline_input
+        current_stage
         stages
-        pipeline_config
-        pipeline_result
     end
     
     methods
-        function obj = Pipeline(pipeline_input, pipeline_config)
-            obj.pipeline_input = pipeline_input;
-            obj.pipeline_config = pipeline_config;
-        end
-        
-        function assemble_pipeline(obj, stage_list)
-            obj.stages = stage_list;
+        function obj = Pipeline()
+            obj.current_stage = -1;
+            obj.stages = {};
         end
         
         function execute(obj)
-            % empty pointer to save prev stage
-            prev = -1;
-            % set first stage input
-            obj.stages{1}.stage_input = obj.pipeline_input;
-            % loop through each stage
+            % stages have to run in sequence, because of result dependency
             for stage = obj.stages
-                % get curr stage
                 curr = stage{:};
-                % get curr stage config by it's class name
-                raw_class_name = split(class(curr), '.');
-                curr.stage_config = obj.pipeline_config.(raw_class_name{end});
-                % set curr stage input using prev stage result
-                if prev ~= -1
-                    curr.stage_input = prev.stage_result;
-                end
-                % execute curr stage 
+                obj.current_stage = curr;
+                disp(obj.current_stage.stage_info);
                 curr.execute();
-                disp(curr); % debug
-                disp(curr.stage_config); % debug
-                % set prev stage pointer to curr stage
-                prev = curr;
             end
-            obj.pipeline_result = prev.stage_result;
         end
     end
 end
--- launch.m
+++ launch.m
@@ -1,18 +1,15 @@
 import src.Utils
+import src.pipeline.RunSpec
 import src.pipeline.Pipeline
-import src.stages.CatGT
-import src.stages.KiloSort
+import src.stage.Stage
+import src.jobs.CatGT
+import src.jobs.KiloSort
 
 %% Static Variables
 PYENV_PATH = 'C:\ProgramData\Anaconda3\envs\spike_sorting\python.exe';
 
-CONFIG_FROM_JSON_PATH = 'configs\config.json';
-% CONFIG_FROM_JSON_PATH = ''; % will generate json by input variables
 
-%% Input Variables
-
-
-%% Pipeline Configuration
+%% Pipeline ENV Configuration
 % load python modules
 pe = pyenv();
 if pe.Status == "NotLoaded"
@@ -21,24 +18,47 @@ end
 caller = py.importlib.import_module('py_modules.caller');
 py.importlib.reload(caller);
 
-% load config file
-if ~strcmp(CONFIG_FROM_JSON_PATH, '')
-    config = Utils.load_json(CONFIG_FROM_JSON_PATH);
-else
-    py.help('py_modules.caller.createInputJson');
-    py.py_modules.caller.createInputJson();
-end
 
-%% Pipeline Definition/Execution
-% set pipeline input
-pipeline = Pipeline('123', config);
+%% Pipeline Input Configuration
+% Input Variables
+% TODO - Set input params
 
-% assemble pipeline by stages
-cat_gt = CatGT;
-kilo_sort = KiloSort;
-pipeline.assemble_pipeline({cat_gt, kilo_sort});
+% Manifest/Information object used to assemble a pipeline
+run_spec = RunSpec();
 
-% run pipeline
+% generate json config file for each job
+% py.help('py_modules.caller.createInputJson');
+% py.py_modules.caller.createInputJson();
+
+
+%% Pipeline Assemble
+% define pipeline
+pipeline = Pipeline();
+% assemble pipeline by stages and jobs
+% TODO - pipeline can be auto-assembled by RunSpec
+% CatGT stage
+stage_catgt = Stage('CatGT');
+% job 0
+job_catgt_0 = CatGT('CatGT probe 0', 'input probe 0');
+stage_catgt.job_queue{end+1} = job_catgt_0;
+% job 1
+job_catgt_1 = CatGT('CatGT probe 1', 'input probe 1');
+stage_catgt.job_queue{end+1} = job_catgt_1;
+% append stage to pipeline
+pipeline.stages{end+1} = stage_catgt;
+
+% KiloSort stage
+stage_kilo = Stage('KiloSort');
+% job 0
+job_kilo_0 = KiloSort('KiloSort probe 0', 'input probe 0');
+stage_kilo.job_queue{end+1} = job_kilo_0;
+% job 1
+job_kilo_1 = KiloSort('KiloSort probe 1', 'input probe 1');
+stage_kilo.job_queue{end+1} = job_kilo_1;
+% append stage to pipeline
+pipeline.stages{end+1} = stage_kilo;
+
+
+%% Pipeline Execution
 pipeline.execute();
-disp(pipeline.pipeline_result);
 
