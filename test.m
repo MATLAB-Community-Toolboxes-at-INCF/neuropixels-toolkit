9b055f7f8ccc6af48c94e87c944e0a022f14e9fb
Drew Yang <drew.yang.dev@gmail.com>
2021-11-30 16:18:21 -0600

    update live script

--- test.m
+++ test.m
@@ -35,78 +35,43 @@ session.parExecute();
 % import npxtoolkit.tasks.CatGT
 % import npxtoolkit.tasks.KiloSort
 % import npxtoolkit.tasks.TPrime
-
-% %% Configuration
-% logger.info("test.m", "Parsing config file for pipeline...");
-% json = Config.parseJson("configs/test_config.json");
-
+% 
 % %% Pipeline 1
-% logger.info("test.m", "Set PipelineConfig...");
-% pipelineConfig1 = PipelineConfig(json.pipeline);
-% pipeline1 = Pipeline('Pipeline0 Info', pipelineConfig1, logger);
-
+% pipeline1 = Pipeline('Pipeline0 Info', "configs/test_config.json");
+% 
 % % CatGT stage
-% logger.info("test.m", "Creating CatGT stage...");
-% stageCatgt = Stage('CatGT', logger);
-% % append stage to pipeline first, in order to pass Pipeline/CommonConfig to tasks
+% stageCatgt = Stage('CatGT');
 % pipeline1.addStage(stageCatgt);
-% logger.info("test.m", "CatGT stage was added!");
 % % CatGT task 1
-% logger.info("test.m", "Creating CatGT task config...");
-% configCatgt1 = TaskConfig(json.CatGT);
-% logger.info("test.m", "Creating CatGT task...");
-% taskCatgt1 = CatGT('CatGT probe 0', '0', 1, configCatgt1, logger);
+% taskCatgt1 = CatGT('CatGT probe 0', '0', 1, pipeline1.PipelineConfigs);
 % stageCatgt.addTask(taskCatgt1);
-% logger.info("test.m", "CatGT task was added!");
-
+% 
 % % KiloSort stage
-% logger.info("test.m", "Creating KiloSort stage...");
-% stageKilo = Stage('KiloSort', logger);
+% stageKilo = Stage('KiloSort');
 % pipeline1.addStage(stageKilo);
-% logger.info("test.m", "KiloSort stage was added!");
 % % KiloSort task 1
-% logger.info("test.m", "Creating KiloSort task config...");
-% configKilo1 = TaskConfig(json.KiloSort);
-% logger.info("test.m", "Creating KiloSort task...");
-% taskKilo1 = KiloSort('KiloSort probe 0', '0', 'cortex', configKilo1, logger);
+% taskKilo1 = KiloSort('KiloSort probe 0', '0', 'cortex', pipeline1.PipelineConfigs);
 % stageKilo.addTask(taskKilo1);
-% logger.info("test.m", "KiloSort task was added!");
-
+% 
 % % TPrime stage
-% logger.info("test.m", "Creating TPrime stage...");
-% stageTPrime = Stage('TPrime', logger);
+% stageTPrime = Stage('TPrime');
 % pipeline1.addStage(stageTPrime);
-% logger.info("test.m", "TPrime stage was added!");
 % % TPrime task 1
-% logger.info("test.m", "Creating TPrime task config...");
-% configTPrime1 = TaskConfig(json.TPrime);
-% logger.info("test.m", "Creating TPrime task...");
-% taskTPrime1 = TPrime('TPrime probe 0', '0', configTPrime1, logger);
+% taskTPrime1 = TPrime('TPrime probe 0', '0', pipeline1.PipelineConfigs);
 % stageTPrime.addTask(taskTPrime1);
-% logger.info("test.m", "TPrime task was added!");
-
+% 
 % % append pipeline to session
-% logger.info("test.m", "Adding the pipeline to session...");
 % session.addPipeline(pipeline1);
 % %% Execution
-% logger.info("test.m", "Executing session...");
 % session.parExecute();
-% logger.info("test.m", "Session execution done!");
 
 
 % %% ===========================================
-% %% Single task excution(test/special usecase)
 % %% Configuration
-% logger.info("test.m", "Parsing config file for pipeline...");
-% json = Config.parseJson("configs/test_config.json");
-
-% pipelineConfig = PipelineConfig(json.pipeline);
-% % config = TaskConfig(json.CatGT);
-% % task = CatGT('CatGT probe 0', '0', 1, config, logger);
+% pipelineConfig = PipelineConfig("configs/test_config.json");
+% task = CatGT('CatGT probe 0', '0', 1, pipelineConfig);
 % % config = TaskConfig(json.KiloSort);
-% % task = KiloSort('KiloSort probe 0', '0', 'cortex', config, logger);
+% % task = KiloSort('KiloSort probe 0', '0', 'cortex', pipelineConfig);
 % % config = TaskConfig(json.TPrime);
-% % task = TPrime('TPrime probe 0', '0', config, logger);
-
-% task.CommonConfig = pipelineConfig;
+% % task = TPrime('TPrime probe 0', '0', pipelineConfig);
 % task.execute();
